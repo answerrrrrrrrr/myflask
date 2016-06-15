6a1ab176941d5c8474a3471c701bd5284db0df8a
answerrrrrrrrr <answerrrrrrrrr@gmail.com>
2016-06-16 07:10:18 +0800

    tests

--- config.py
+++ config.py
@@ -3,21 +3,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    SQLALCHEMY_COMMIT_ON_TEARDOWN = True  # auto commit after db.session.add
-
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     MAIL_SERVER = 'applesmtp.163.com'
     MAIL_PORT = 0
     MAIL_USE_TLS = True
     MAIL_USERNAME = 'huamingrui@163.com'
     MAIL_PASSWORD = '3322323'
-    FLASKY_ADMIN = 'huamingrui@163.com'
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_MAIL_SUBJECT_PREFIX = '[MrHua]'
     FLASKY_MAIL_SENDER = 'MrHua <huamingrui@163.com>'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASKY_ADMIN = 'huamingrui@163.com'
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
@@ -37,6 +33,7 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
 
 
 class ProductionConfig(Config):
